e373e7d9fe3a96d572de19c7ccd899b84d14fcde
Simon Scarfe <simon.scarfe@gmail.com>
2015-06-17 21:31:27 +0100

    correctly evolves a blank grid

--- test/GameOfLifeSpec.js
+++ test/GameOfLifeSpec.js
@@ -1,8 +1,14 @@
+/* global it */
 /* global describe */
 var assert = require('assert');
 
 var gameOfLife = require('../lib/GameOfLife');
 
 describe('GameOfLife', function () {
-	
+	describe('#evolve', function () {
+		it('should return a blank grid when given a blank grid', function () {
+			var result = gameOfLife.evolve([[0, 0], [0, 0]]);
+			assert.deepEqual([[0, 0], [0, 0]], result);
+		});
+	});
 });
